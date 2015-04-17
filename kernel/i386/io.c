39d81fda668ab9019ea7007f3d99cd613af92ebc
tomoyat1 <tomoyat1@gmail.com>
2015-04-17 21:33:19 +0900

    -c -std=gnull -isystem/Users/tomoyat1/src/tomos/sysroot/usr/include/usr/include -ffreestanding -Wall -Wextra  -T /linker.ld -ffreestanding -Wall -Wextra -nostdlib -lk -lgcc

--- kernel/i386/io.c
+++ kernel/i386/io.c
@@ -1,12 +1,13 @@
 #include <kernel/io.h>
-
+#include <stddef.h>
+#include <stdint.h>
 
 void outb(uint16_t ioport, uint8_t output)
 {
-	__asm__(
-		
-		
-
+	__asm__ __volatile__(
+		"outb %1, %2"
+		:
+		:"r"(ioport), "r"(output)	
 	);
 
 }
