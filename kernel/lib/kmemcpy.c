fb86e456cb6e039d051d871b319fd8423283c62a
tomoyat1 <tomoyat1@gmail.com>
2015-04-20 18:26:21 +0900

    kmemcpy.c

---
+++ kernel/lib/kmemcpy.c
@@ -0,0 +1,22 @@
+/*
+ *
+ *   kmemcpy.c
+ *   kmemcpy function for the kernel
+ *
+ *
+ */
+
+#include <stddef.h>
+
+#include <kernel/kmemcpy.h> 
+
+void* kmemcpy(void *dest, void *src, size_t num)
+{
+	char *dest8 = (char *)dest;
+	char *src8 = (char *)src;
+	for ( int i = 0; i < num; i++ )
+	{
+		src8[i] = dest8[i];
+	}
+	return dest;
+}
---
+++ sysroot/usr/include/kernel/kmemcpy.h
@@ -0,0 +1,7 @@
+/*
+ *
+ *    kmemcpy.h
+ *
+ */
+
+ void* kmemcpy(void *dest, void *src, size_t num);
