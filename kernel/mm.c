819fddcc3c75f485a5420a61008df97da48909d7
tomoyat1 <tomoyat1@gmail.com>
2015-04-30 11:04:07 +0900

    mm.h

---
+++ kernel/mm.c
@@ -0,0 +1,13 @@
+/*
+ *
+ * memory management. get free memory, release memory, all the good stuff. 
+ *
+ *
+ *
+ */
+
+
+#include <kernel/mm.h>
+
+#include <stddef.h>
+#include <stdint.h>
---
+++ sysroot/usr/include/kernel/mm.h
@@ -0,0 +1,21 @@
+#ifndef _MM_H_
+#define _MM_H_
+
+#include <stddef.h>
+#include <stdint.h>
+
+typedef struct free_mem free_mem_t;
+
+typedef struct free_mem
+{
+	size_t size;
+	char* start_addr;
+	free_mem_t* next;
+	free_mem_t* prev;
+} free_mem_t;
+
+
+void initmm();
+
+free_mem_t* search_free_mem(size_t bytes);
+#endif
