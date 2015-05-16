ede6de6558651e899ef851dd80925049cb711126
tomoyat1 <tomoyat1@gmail.com>
2015-05-16 09:51:00 +0900

    Fixed kstrcpy not making null terminated string.

--- kernel/lib/kstrcpy.c
+++ kernel/lib/kstrcpy.c
@@ -10,10 +10,10 @@ char *kstrcpy(char *s1, const char *s2)
 	char *dest = s1;
 	const char *src = s2;
 	int i = 0;
-	while (s2[i] != '\0') {
+	//Gotta append \0 at end
+	for (i = 0; s2[i] != '\0'; i++)
 		dest[i] = src[i];
-		i++;
-	}
+	dest[i] = '\0';
 	return dest;
 }
 
