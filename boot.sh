f7219a6cd1e43ad7385dd50e777db4b7a3352689
Tomoya Tabuchi <t@tomoyat1.com>
2015-12-19 19:29:54 +0900

    misc build system changes

--- boot.sh
+++ boot.sh
@@ -1,3 +1,3 @@
-qemu-system-i386 -kernel sysroot/boot/kernel -monitor stdio -cpu Broadwell -d cpu_reset,int -no-reboot -m 4G
+qemu-system-i386 -kernel sysroot/boot/kernel -monitor stdio -cpu Broadwell -d cpu_reset,int -no-reboot -m 4G -s -S
 
 #-d cpu_reset,int -no-reboot
